2099e7ece71b90818b25acbc1f98e7e82bb5ea08
Guilherme Alves <guiigos.alves@gmail.com>
2026-03-29 13:52:16 -0300

    feat: refactoring services

--- src/modules/academic/students/students.module.ts
+++ src/modules/academic/students/students.module.ts
@@ -1,23 +1,15 @@
-import { CreateStudentService } from "@academic/students/application/services/create-student.service";
-import { EditStudentService } from "@academic/students/application/services/edit-student.service";
-import { ListStudentsService } from "@academic/students/application/services/list-students.service";
-import { RemoveStudentService } from "@academic/students/application/services/remove-student.service";
-import { ReturnStudentService } from "@academic/students/application/services/return-student.service";
 import { STUDENT_REPOSITORY } from "@academic/students/domain/repositories/student-repository.interface";
 import { StudentsController } from "@academic/students/infra/controllers/students.controller";
 import { DrizzleStudentRepository } from "@academic/students/infra/repositories/drizzle-student.repository";
 import { DatabaseModule } from "@infra/database/database.module";
 import { Module } from "@nestjs/common";
+import { StudentService } from "./application/services/student.service";
 
 @Module({
   imports: [DatabaseModule],
   controllers: [StudentsController],
   providers: [
-    CreateStudentService,
-    EditStudentService,
-    ListStudentsService,
-    ReturnStudentService,
-    RemoveStudentService,
+    StudentService,
     DrizzleStudentRepository,
     {
       provide: STUDENT_REPOSITORY,
